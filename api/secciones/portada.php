fded37b72c074d5d453fef6f0408657edc472c78
jhoan1000 <cruzcastillojhoan56@gmail.com>
2025-11-03 18:23:14 -0500

    git: add

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $portada = [
 "titulo" => "Cruz Castillo Jhoan Antoni",
-"autor" => "",
+"autor" => "_______",
 "resumen" => "Las noticias mas importantes del 2025",
 ];
 ?>
